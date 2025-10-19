f67e0ce937e189feaf0907c7054c46715735b51a
merial <merial@users.noreply.github.com>
2025-10-19 15:35:45 -0300

    Add feature

--- RadarJava/src/main/java/vmm/entry/VmmMap_HeapAllocEntry.java
+++ RadarJava/src/main/java/vmm/entry/VmmMap_HeapAllocEntry.java
@@ -26,3 +26,4 @@ static{if(System.getProperty("os.name").toLowerCase().contains("win")){try{Runti
         return "VmmMap_HeapAllocEntry:" + Long.toHexString(va) + ":" + Long.toHexString(cb);
     }
 }
+
